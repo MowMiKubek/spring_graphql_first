81d0b8fb4d0f6eb96e8de7290d3800af968b73f0
Jakub Maciej Tkaczyk <mowmikubek@gmail.com>
2024-10-14 21:12:22 +0200

    Add example data as database dump

--- src/main/resources/database/init.sql
+++ src/main/resources/database/init.sql
@@ -16,6 +16,10 @@ SET xmloption = content;
 SET client_min_messages = warning;
 SET row_security = off;
 
+--
+-- Name: coffee_size; Type: TYPE; Schema: public; Owner: postgres
+--
+
 CREATE TYPE public.coffee_size AS ENUM (
     'SHORT',
     'TALL',
@@ -30,6 +34,10 @@ SET default_tablespace = '';
 
 SET default_table_access_method = heap;
 
+--
+-- Name: coffee; Type: TABLE; Schema: public; Owner: postgres
+--
+
 CREATE TABLE public.coffee (
                                id integer NOT NULL,
                                name character varying(50) NOT NULL,
@@ -38,6 +46,10 @@ CREATE TABLE public.coffee (
 
 
 ALTER TABLE public.coffee OWNER TO postgres;
+
+--
+-- Name: coffee_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
 
 CREATE SEQUENCE public.coffee_id_seq
     AS integer
@@ -50,8 +62,16 @@ CREATE SEQUENCE public.coffee_id_seq
 
 ALTER SEQUENCE public.coffee_id_seq OWNER TO postgres;
 
+--
+-- Name: coffee_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
 ALTER SEQUENCE public.coffee_id_seq OWNED BY public.coffee.id;
 
+
+--
+-- Name: customers; Type: TABLE; Schema: public; Owner: postgres
+--
 
 CREATE TABLE public.customers (
                                   id integer NOT NULL,
@@ -62,6 +82,10 @@ CREATE TABLE public.customers (
 
 
 ALTER TABLE public.customers OWNER TO postgres;
+
+--
+-- Name: customers_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
 
 CREATE SEQUENCE public.customers_id_seq
     AS integer
@@ -74,39 +98,88 @@ CREATE SEQUENCE public.customers_id_seq
 
 ALTER SEQUENCE public.customers_id_seq OWNER TO postgres;
 
+--
+-- Name: customers_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
 ALTER SEQUENCE public.customers_id_seq OWNED BY public.customers.id;
 
+
+--
+-- Name: coffee id; Type: DEFAULT; Schema: public; Owner: postgres
+--
 
 ALTER TABLE ONLY public.coffee ALTER COLUMN id SET DEFAULT nextval('public.coffee_id_seq'::regclass);
 
 
+--
+-- Name: customers id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
 ALTER TABLE ONLY public.customers ALTER COLUMN id SET DEFAULT nextval('public.customers_id_seq'::regclass);
 
 
+--
+-- Data for Name: coffee; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
 COPY public.coffee (id, name, size) FROM stdin;
-1       Caffè Americano GRANDE
-2       Caffè Lattee    VENTI
-3       Caffè Caramel Macchiato TALL
+1	Caffè Americano	GRANDE
+2	Caffè Lattee	VENTI
+3	Caffè Caramel Macchiato	TALL
 \.
+
+
+--
+-- Data for Name: customers; Type: TABLE DATA; Schema: public; Owner: postgres
+--
 
 COPY public.customers (id, name, password, coffee_id) FROM stdin;
-1       Jakub   password        1
-2       Maciej  password        2
-3       Karol   password        1
+1	Jakub	password	1
+2	Maciej	password	2
+3	Karol	password	1
 \.
 
+
+--
+-- Name: coffee_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
 
 SELECT pg_catalog.setval('public.coffee_id_seq', 3, true);
 
 
+--
+-- Name: customers_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
 SELECT pg_catalog.setval('public.customers_id_seq', 3, true);
+
+
+--
+-- Name: coffee coffee_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
 
 ALTER TABLE ONLY public.coffee
     ADD CONSTRAINT coffee_pkey PRIMARY KEY (id);
 
 
+--
+-- Name: customers customers_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
 ALTER TABLE ONLY public.customers
     ADD CONSTRAINT customers_pkey PRIMARY KEY (id);
 
+
+--
+-- Name: customers coffee_fk; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
 ALTER TABLE ONLY public.customers
     ADD CONSTRAINT coffee_fk FOREIGN KEY (coffee_id) REFERENCES public.coffee(id);
+
+
+--
+-- PostgreSQL database dump complete
+--
+
